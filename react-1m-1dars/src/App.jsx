24ee34d1e229c891283b187951191f7b68e48628
Zafar <ergashevzafar1777@gmail.com>
2025-10-03 07:44:37 +0500

    react-1m-1dars

---
+++ react-1m-1dars/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <div>
+     <h1>Welcome to React JS</h1>
+     <br />
+     <h2>This is my first React app</h2>
+     <br />
+     <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>React</li>
+     </ul>
+     <br />
+     <h3>what is React</h3>
+     <br />
+     <p>React nima? React - bu Facebook tomonidan yaratilgan ochiq kodli JavaScript kutubxonasi bolib,
+      asosan foydalanuvchi interfeyslarini (UI) yaratish uchun moljallangan. U yagona sahifali
+      ilovalar (single-page applications) va dinamik, sezgir veb-ilovalarni ishlab chiqishda keng qollaniladi.
+     </p>
+     <h4>What is Single-page application (SPA)</h4>
+     <p>SPA (Single-Page Application) - bu bitta sahifali veb-ilova bolib,unda sahifa kontenti dinamik
+      rvishda yangilanadi va foydalanuvchining har bir amalida butun sahifani qaytadan yuklash talab qilinmaydi.
+      JavaScript yordamida faqat kerakli kontent ozgartiriladi, bu esa ilovani tezkor va foydalanuvchi uchun qulay qiladi.   </p>
+    </div>
+      
+    </div>
+  )
+}
+
+export default App
